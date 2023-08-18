f1d7021f849a9e73731df9a7da5150e141fa6fb2
Paulina <paulinasaldarriaga@gmail.com>
2023-08-17 19:06:53 -0500

    ejemploFormulario

--- ejemplos/index.js
+++ ejemplos/index.js
@@ -1,28 +1,23 @@
-//Array
-const estudiantes = []
 
-//agregar un elemento
-// estudiantes.push(5)
-// estudiantes.push(null)
-// estudiantes.push({})
-// estudiantes.push(()=>(console.log()))
+const getDataForm =() => {
+    const txtName = document.getElementById("txtName").value;
+    const txtemail = document.getElementById("txtEmail").value;
+    const txtDesciption = document.getElementById("txtDesciption").value;
+    const acceptConditions = document.getElementById("acceptCondition").checked;
+    const typeA = document.getElementById("typeA").checked;
+    const typeB = document.getElementById("typeB").checked;
+    
 
-estudiantes.push({"nombre": "Juan", id:123})
-estudiantes.push({"nombre": "Maria", id:1234})
-
-estudiantes.forEach((x)=>console.log(x.nombre))
-
-for(let i=0; i<estudiantes.length; i++){
-    if (estudiantes[i].nombre == "Maria"){
-        console.log(estudiantes[i].id);
+    //JSON {clave:valor}
+    const data = {
+        name:txtName,
+        email:txtemail,
+        description:txtDesciption,
+        acceptCondition: acceptConditions,
+        typeA,
+        typeB
     }
+
+    console.log(data);
 }
 
-const maria = estudiantes.find((x)=>x.nombre == "Maria")
-console.log(estudiantes);
-
-const marias = estudiantes.filter((x)=>x.nombre == "Maria")
-marias[0].id
-
-estudiantes = estudiantes.map(x=>{return{...x, edad: null}})
-console.log(estudiantes);
